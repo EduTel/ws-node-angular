c6c2dbfa07880140eba7d35771d97312f5477813
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-04-27 12:53:58 -0500

    pug

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
+const pug   = require('pug');
 var app     = express();
 // GET method route
 app.get('/', function (req, res) {
-    res.send('GET request to the homepage');
+    res.send(pug.render('p Hello world!'));
 });
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
 // POST method route
 app.post('/', function (req, res) {
-    res.send('POST request to the homepage');
+    res.send(pug.render('p Hello world!'));
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
--- index_crudo.js
+++ index_crudo.js
@@ -5,9 +5,9 @@ const port = 3000;
 var arreglo_parametros = [];
 
 const server = http.createServer((req, res) => {
-    if ( req.url.indexOf("?">0) ) {
-         var url_data = req.url.split("?");
-         var arreglo_parametros = url_data[1].split("&");
+    if (req.url.indexOf("?" > 0)) {
+        var url_data = req.url.split("?");
+        var arreglo_parametros = url_data[1].split("&");
     }
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
