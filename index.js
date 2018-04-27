079ba39cbcda3e18ed1091cd0b3208c17a42a29b
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-04-27 17:33:16 -0500

    renderFile

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-var express = require('express');
-const pug   = require('pug');
-var app     = express();
+var express = require('express');//manejador de servidor
+var app = express();
+
+var pug = require('pug'); //motor de vista
+app.set('views', 'view');
+app.set('view engine', 'pug');
+
+var get_estados = [[1, 2, 3], [4, 5, 6], [7]];
+p_view = {
+        h1        : 'Estados 1',
+        estados_p : get_estados
+};
+// Compile the source code
+const view = pug.renderFile('view/index.pug', p_view);
 // GET method route
 app.get('/', function (req, res) {
-    res.send(pug.render('p Hello world!'));
+    res.render('index', {
+        h1: 'Estados 2',
+    });
+    //res.send(view);
 });
-//app.get('/', (req, res) => res.send('Hello World!'))
 // POST method route
-app.post('/', function (req, res) {
-    res.send(pug.render('p Hello world!'));
-});
+//app.post('/', function (req, res) {
+//    res.send(view);
+//});
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
