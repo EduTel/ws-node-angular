8815aa7a992a40bdae028f8bff739bd7e2ad534c
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-05-03 18:48:00 -0500

    sass

--- index.js
+++ index.js
@@ -4,46 +4,62 @@ var app = express();
 var pug = require('pug'); //motor de vista
 app.set('views', 'view');
 app.set('view engine', 'pug');
-
+/*************************sass****************************/
 var connect        = require('connect');
 var sassMiddleware = require('node-sass-middleware');
-var srcPath = '_sass/index.sass';
-var destPath = '_styles/';
-//app.use('styles/', sassMiddleware);
+var path           = require('path');
+//var winston        = require('winston');
+//winston.level = 'debug';
+//var srcPath = 'sass/index.sass';
+var destPath = 'sass/';
 app.use(
     sassMiddleware({
-        src         : srcPath,
-        dest        : destPath,
-        debug       : true,
-        outputStyle : 'expanded'
+        /* Options */
+        src            : "compile/sass/",
+        dest           : "css",
+        debug          : true,
+        outputStyle    : 'compressed',
+        indentedSyntax : true,
+        //prefix: __dirname
+        //response      : true
+        //prefix: '/prefix' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/
+        //log: function (severity, key, value) {
+        //    console.log(severity, 'Error sass:', key, value);
+        //}
     })
 );
-var get_estados = [[1, 2, 3],[4, 5, 6], [7]];
-
+/*********************************************************/
 // GET method route
-app.get('/', function (req, res) {
-    res.redirect('/estados');
-});
-app.get('/:algo', function (req, res) {
+//app.get('/', function (req, res, next) {
+//    res.redirect('/Estados');
+//});
+//app.get('/:algo', function (req, res, next) {
+app.get('/', function (req, res, next) {
     //res.render('index', {
     //    h1: 'Estados 2',
     //});
+    var get_estados = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7]
+    ];
     p_view = {
-        titulo: req.params.algo,
-        estados_p: get_estados
+        //titulo    : req.params.algo,
+        titulo: 'Estados',
+        estados_p : get_estados
     };
     // Compile the source code
     var index = pug.renderFile('view/index.pug', p_view);
-    var view = pug.renderFile('view/header.pug', {
+    var view  = pug.renderFile('view/header.pug', {
         contenido: index
     });
     res.send(view);
 });
 // POST method route
-app.post('/', function (req, res) {
+app.post('/', function (req, res, next) {
     res.send(view);
 });
 app.listen(3000, function () {
-    console.log("url: "+__dirname);
-    console.log('Example app listening on port 3000!');
+    //console.log("url: "+__dirname);
+    //console.log('Example app listening on port 3000!');
 });
