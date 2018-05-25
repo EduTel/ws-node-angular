38c3606001d1be6a0b82c17ad5a7169e474d895e
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-05-25 10:48:21 -0500

    eliminar js y .map

--- outDir/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("./config/express");
-const app = express_1.express_module.__express();
-app.listen(3000, function () {
-    console.log("**********************************Iniciando servidor express**********************************");
-    console.log("url: " + __dirname);
-});
-//# sourceMappingURL=server.js.map
