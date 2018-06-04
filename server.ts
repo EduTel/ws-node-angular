b4c69b990293d7cf07550910ded9694cac2b4403
EduardoTel <15623704+EduTel@users.noreply.github.com>
2018-06-04 16:57:29 -0500

    angular cambio de arquitectura

--- server.ts
+++
@@ -1,6 +0,0 @@
-import { express_module} from './config/express';
-const app = express_module.__express();
-app.listen(3000, function () {
-    console.log("**********************************Iniciando servidor express**********************************");
-    console.log("url: " + __dirname);
-});
